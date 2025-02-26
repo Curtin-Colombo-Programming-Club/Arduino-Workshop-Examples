e7529b5450225469683e3e2ed9cc72d897603f1e
HellFireInfernoStorm <dinura2003@gmail.com>
2025-02-27 03:40:44 +0530

    Add move_motors.ino
    Add a code example to move motors

---
+++ move_motors/move_motors.ino
@@ -0,0 +1,84 @@
+// ================================================================================
+// Author: Dinura Silva
+// Project: Robotics workshop
+// Description: Control 2 motors using L298N Motor Driver
+// ================================================================================
+
+// Set the constants for motor driver pins
+const int MOTOR1PIN1 = 2;
+const int MOTOR1PIN2 = 3;
+const int MOTOR2PIN1 = 4;
+const int MOTOR2PIN2 = 5;
+
+void setup() {
+  // Begin Serial communication
+  Serial.begin(9600);
+
+  // Set modes for motor driver's pins
+  pinMode(MOTOR1PIN1, OUTPUT);
+  pinMode(MOTOR1PIN2, OUTPUT);
+  pinMode(MOTOR2PIN1, OUTPUT);
+  pinMode(MOTOR2PIN2, OUTPUT);
+
+  // Default all pins to low (disable all motors)
+  digitalWrite(MOTOR1PIN1, LOW);
+  digitalWrite(MOTOR1PIN2, LOW);
+  digitalWrite(MOTOR2PIN1, LOW);
+  digitalWrite(MOTOR2PIN2, LOW);
+}
+
+void loop() {
+  // Make some movements 
+  moveForward();
+  delay(20);
+  moveBackward();
+  delay(20);
+  turnRight();
+  delay(20);
+  turnLeft();
+  delay(20);
+}
+
+// Move both wheels forward
+void moveForward() {
+  digitalWrite(MOTOR1PIN1, LOW);
+  digitalWrite(MOTOR1PIN2, LOW);
+  digitalWrite(MOTOR2PIN1, LOW);
+  digitalWrite(MOTOR2PIN2, LOW);
+}
+
+// Move both wheels backwards
+void moveBackwards() {
+  digitalWrite(MOTOR1PIN1, LOW);
+  digitalWrite(MOTOR1PIN2, HIGH);
+  digitalWrite(MOTOR2PIN1, LOW);
+  digitalWrite(MOTOR2PIN2, HIGH);
+}
+
+// Start rotating clockwise (turning right)
+void turnRight() {
+  // Left motor forwards, right motor backwards
+  digitalWrite(MOTOR1PIN1, HIGH);
+  digitalWrite(MOTOR1PIN2, LOW);
+  digitalWrite(MOTOR2PIN1, LOW);
+  digitalWrite(MOTOR2PIN2, HIGH);
+}
+
+// Start rotating counter-clockwise (turning left)
+void turnLeft() {
+  // Left motor backwards, right motor forwards
+  digitalWrite(MOTOR1PIN1, LOW);
+  digitalWrite(MOTOR1PIN2, HIGH);
+  digitalWrite(MOTOR2PIN1, HIGH);
+  digitalWrite(MOTOR2PIN2, LOW);
+}
+
+// Stop moving motors
+void stopMotors() {
+  // Setting both pins LOW will stop a motor
+  digitalWrite(MOTOR1PIN1, LOW);
+  digitalWrite(MOTOR1PIN2, LOW);
+
+  digitalWrite(MOTOR2PIN1, LOW);
+  digitalWrite(MOTOR2PIN2, LOW);
+}
